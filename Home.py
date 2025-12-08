d663d47acb8494c5d8d6f7fc44df5ba64dcf1278
heuristic_machina <machine_learner@yahoo.com>
2025-12-07 20:55:35 -0700

    emoji sidebar

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title='Airline Satisfaction Predictions', page_icon='✈
 
 st.title('✈️ Airline Satisfaction Dataset Explorer')
 st.subheader('Welcome to the Airline Satisfaction dataset explorer app')
-st.write('This app provides an interactive platform to explore traveler satisfaction through the pages on the sidebar.')
+st.write('👈 This app provides an interactive platform to explore traveler satisfaction through the pages on the sidebar.')
 st.write('You can visualize the distribution of data in 🛢 Data Overview and 📖 Dictionary pages, explore relationships between features on 📐 Exploratory Analysis page, and even make your own confusion matrix model on 𝄜 Make Your Own Matrix page!')
 st.write('When your done exploring, fly over to the 🎯 Prediction Insights page for experimental results performed on this dataset using Machine Learning Classifying Models.')
 st.image('imgs/airline.jpg', caption="Airline - Vecteezy.com")
